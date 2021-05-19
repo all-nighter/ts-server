0344b46ca11ae49ffb35a35351823cf1b5210bbe
Woosik Kim <iam.woosik.kim@gmail.com>
2021-05-19 16:33:30 +0900

    Use mongoose

--- src/env.ts
+++ src/env.ts
@@ -2,3 +2,7 @@ require('dotenv').config()
 import env from 'env-var'
 
 export const PORT: number = env.get('PORT').required().asPortNumber()
+export const MONGODB_HOST: string = env
+  .get('MONGODB_HOST')
+  .required()
+  .asString()
